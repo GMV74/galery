ecd45dfc7345506b756cb02b0e5c8400d5e1117d
GMV74 <62757252+GMV74@users.noreply.github.com>
2020-03-27 09:53:02 -0500

    Create mi.js

---
+++ mi.js
@@ -0,0 +1,19 @@
+        fetch('gradiAuthors.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+
+        function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = '<img src="' + data[i].download_url + '">';
+                mainContainer.appendChild(div);
+            }
+        }
